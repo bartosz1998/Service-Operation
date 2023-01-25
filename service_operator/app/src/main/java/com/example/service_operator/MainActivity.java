15a5923d260ba580ef8e3017a7cb4a38562ed3e0
bartosz <bartoszbizon1998@gmail.com>
2023-01-25 18:10:50 +0100

    trying add data binding and fragment

--- service_operator/app/src/main/java/com/example/service_operator/MainActivity.java
+++ service_operator/app/src/main/java/com/example/service_operator/MainActivity.java
@@ -6,12 +6,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button firstFragmentBtn, secondFragmentBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- service_operator/app/src/main/java/com/example/service_operator/fragment1.java
+++ service_operator/app/src/main/java/com/example/service_operator/fragment1.java
@@ -4,27 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentResultListener;
+
+import com.example.service_operator.databinding.FragmentBinding;
 
 public class fragment1 extends Fragment {
-    View view;
 
+    FragmentBinding binding;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-
-        // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_fragment1, container, false);
-
-
-
-        return view;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment, container, false);
+        Item item = new Item();
+        item.setName("Thomas");
+        binding.setItem(item);
+        return binding.getRoot();
     }
 }
